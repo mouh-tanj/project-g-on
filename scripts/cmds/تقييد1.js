8f5d0909eff9235099a588fa4bbecc986bfb9229
mouhamedalger <154691398+mouhamedalger@users.noreply.github.com>
2024-08-30 09:29:54 +0100

    تقييد1.js

--- scripts/cmds/تقييد1.js
+++ scripts/cmds/تقييد1.js
@@ -11,25 +11,16 @@ module.exports = {
 		countDown: 5,
 		role: 2,
 		description: {
-			vi: "bật/tắt chế độ chỉ admin mới có thể sử dụng bot",
 			en: "turn on/off only admin can use bot"
 		},
 		category: "owner",
 		guide: {
-			vi: "   {pn} [on | off]: bật/tắt chế độ chỉ admin mới có thể sử dụng bot"
-				+ "\n   {pn} noti [on | off]: bật/tắt thông báo khi người dùng không phải là admin sử dụng bot",
 			en: "   {pn} [on | off]: turn on/off the mode only admin can use bot"
 				+ "\n   {pn} noti [on | off]: turn on/off the notification when user is not admin use bot"
 		}
 	},
 
 	langs: {
-		vi: {
-			turnedOn: "Đã bật chế độ chỉ admin mới có thể sử dụng bot",
-			turnedOff: "Đã tắt chế độ chỉ admin mới có thể sử dụng bot",
-			turnedOnNoti: "Đã bật thông báo khi người dùng không phải là admin sử dụng bot",
-			turnedOffNoti: "Đã tắt thông báo khi người dùng không phải là admin sử dụng bot"
-		},
 		en: {
 			turnedOn: "Turned on the mode only admin can use bot",
 			turnedOff: "Turned off the mode only admin can use bot",
